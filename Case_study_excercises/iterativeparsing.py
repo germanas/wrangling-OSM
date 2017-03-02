5813497768d004710a5448930f5970fb1d163434
Germanas <germanas.latvaitis@gmail.com>
2017-03-02 22:31:31 +0200

    added excercises folder

---
+++ Case_study_excercises/iterativeparsing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Your task is to use the iterative parsing to process the map file and
+find out not only what tags are there, but also how many, to get the
+feeling on how much of which data you can expect to have in the map.
+Fill out the count_tags function. It should return a dictionary with the
+tag name as the key and number of times this tag can be encountered in
+the map as value.
+
+Note that your code will be tested with a different data file than the 'example.osm'
+"""
+import xml.etree.cElementTree as ET
+import pprint
+
+def count_tags(filename):
+    tagsDict = {}
+    for event, elem in ET.iterparse(filename):
+        if elem.tag not in tagsDict:
+            tagsDict[elem.tag] = 1
+        else:
+            tagsDict[elem.tag] += 1
+
+    # iterparse loops through each of the elements
+    # of the file, one by one, let's print the tag of each element:
+    return tagsDict
+
+
+pprint.pprint(count_tags("vilniussample.osm"))
